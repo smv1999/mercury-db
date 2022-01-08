1d88ddae04cd2cac6425461f6b08ab018eda866a
Vaidhyanathan S M <vaidhyanathan.sm@gmail.com>
2022-01-08 22:55:23 +0530

    Initialised Repo

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mercury-db',
+    version='0.0.1',
+    description='The mercury-db package is a simple key-value datastore',
+    packages=['mercury-db'],
+    py_modules=['py_dsa.mercury-db'],
+    package_dir={'': 'src'},
+    extras_require={
+        "dev": [
+            "pytest >= 3.7",
+            "check-manifest",
+            "twine"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Vaidhyanathan S M",
+    author_email="vaidhyanathan.sm@gmail.com",
+    url="https://github.com/smv1999/mercury-db"
+)
